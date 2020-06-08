a17dd512b985313e453bece2d289d6e54157bc50
filonovsv <36759848+filonovsv@users.noreply.github.com>
2020-06-08 10:15:25 +0700

    Delete Welcom.java

--- lab2/Welcom.java
+++
@@ -1,6 +0,0 @@
-public class Welcome {
-    public static void main(String[] args) {
-        // Показать в консоли сообщение Welcome to Java!
-        System.out.println("Welcome to Java!");
-    }
-}
